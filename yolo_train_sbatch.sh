0edf1cfca439c8241800943f621408cb11b4500b
maja neo <nmaja@mscluster-login1.ms.wits.ac.za>
2026-01-02 17:54:18 +0200

    Cluster related changes

--- yolo_train_sbatch.sh
+++ yolo_train_sbatch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH--job-name=training_yolo
+#SBATCH --job-name=training_yolo
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH--output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_results.txt
-#SBATCH --error=logs/%x_%j.err
+#SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_results.txt
+#SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_errors.txt
+#SBATCH --partition=bigbatch
 sleep 30
 
-torchrun --nproc_per_node=2 execute_training.py
+torchrun --nproc_per_node=1 execute_training.py
