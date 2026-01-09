7b58052d84e1894a6bd07085d9316afc791da0ac
neozakg26 <Neokgmaja26@gmail.com>
2026-01-09 12:04:12 +0200

    sbatch update

--- run_dp_tracker_sbatch.sh
+++ run_dp_tracker_sbatch.sh
@@ -2,12 +2,8 @@
 #SBATCH --job-name=tracker
 #SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_tracker_results.txt
 #SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_tracker_errors.txt
-#SBATCH --array=0-119%6
 N=$(wc -l < scenes.txt)
 #SBATCH --array=0-$(($N-1))%6 run_tracker.sbatch
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=bigbatch
