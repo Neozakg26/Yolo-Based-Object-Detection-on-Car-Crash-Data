15b06d785a03e3287b3f52a8ab135e9638930086
neozakg26 <Neokgmaja26@gmail.com>
2026-01-26 16:52:07 +0200

    remove gres

--- run_dp_tracker_sbatch.sh
+++ run_dp_tracker_sbatch.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=tracker
 #SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_tracker_results.txt
 #SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/sbatch_tracker_errors.txt
-#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=bigbatch
