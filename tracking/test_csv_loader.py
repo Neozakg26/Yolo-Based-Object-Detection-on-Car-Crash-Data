23fbb33b984c7216cc319561b2160291928f0d53
neozakg26 <Neokgmaja26@gmail.com>
2026-01-26 14:27:35 +0200

    testing CrashTable.csv

--- tracking/test_csv_loader.py
+++ tracking/test_csv_loader.py
@@ -1,7 +1,7 @@
 from explainability.metadata import MetaData
 
 if __name__ == "__main__":
-    PATH = "C:/Users/neokg/Coding_Projects/yolo-detector/car_crash_dataset/CCD_images/Crash_Table.csv"
+    PATH = "/datasets/nmaja/Crash_Table.csv"
 
     df = MetaData(PATH,scene_id="000001")
 
