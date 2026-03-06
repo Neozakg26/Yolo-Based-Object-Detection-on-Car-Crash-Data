fe8385070e9cb3a1510f2eb8aa1b9764b22bbe44
neozakg26 <Neokgmaja26@gmail.com>
2026-03-07 00:37:27 +0200

    change pat h

--- run_prediction.sh
+++ run_prediction.sh
@@ -9,9 +9,9 @@
 echo "Starting Accident Prediction"
 
 DATASET="/datasets/nmaja/CrashBest/new_results"
-MODEL_PATH="/datasets/nmaja/CrashBest/results/global_risk_model.parquet"
+MODEL_PATH="/datasets/nmaja/CrashBest/results/global_risk_model.classifier.pkl"
 SCENE_CSV="scene_index.csv"
 
-python3 -m execute.predict_accident --results_dir $DATASET --model_path  $MODEL_PATH --scene_labels $SCENE_CSV
+python3 -m execute.predict_accident --results_dir $DATASET --model_path  $MODEL_PATH --scene_labels $SCENE_CSV 
 
 echo "Fnishing Accident Prediction"
