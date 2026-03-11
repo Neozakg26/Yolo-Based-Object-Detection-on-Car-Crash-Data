2f6d9af6b1e8eac3efb777f41266fc189a3cd2e9
neozakg26 <Neokgmaja26@gmail.com>
2026-03-11 05:17:16 +0200

    fold 2

--- run_prediction.sh
+++ run_prediction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=predict
-#SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_out_fold_01.txt
-#SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_err_fold_01.txt
+#SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_out_fold_02.txt
+#SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_err_fold_02.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=bigbatch
@@ -9,8 +9,8 @@
 echo "Starting Accident Prediction"
 
 DATASET="/datasets/nmaja/CrashBest/new_results"
-MODEL_PATH="/datasets/nmaja/CrashBest/results/eval_plots_cv/fold_01/global_model_fold_01.parquet"
-CLASSIFIER="global_model_fold_01.classifier.pkl"
+MODEL_PATH="/datasets/nmaja/CrashBest/results/eval_plots_cv/fold_02/global_model_fold_02.parquet"
+CLASSIFIER="global_model_fold_02.classifier.pkl"
 SCENE_CSV="scene_index.csv"
 
 python3 -m execute.predict_accident --results_dir $DATASET --model_path  $MODEL_PATH --scene_labels $SCENE_CSV --k 3 --thr 75
