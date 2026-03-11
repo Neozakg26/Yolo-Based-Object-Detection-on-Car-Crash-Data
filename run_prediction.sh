c43a6e2e1394bb5a6fa52874a4ebfa3cd6629a39
neozakg26 <Neokgmaja26@gmail.com>
2026-03-12 00:15:23 +0200

    fold 4

--- run_prediction.sh
+++ run_prediction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=predict
-#SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_out_fold_03.txt
-#SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_err_fold_03.txt
+#SBATCH --output=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_out_fold_04.txt
+#SBATCH --error=/home-mscluster/nmaja/Yolo-Based-Object-Detection-on-Car-Crash-Data/prediction_err_fold_04.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=bigbatch
@@ -9,7 +9,7 @@
 echo "Starting Accident Prediction"
 
 DATASET="/datasets/nmaja/CrashBest/new_results"
-MODEL_PATH="/datasets/nmaja/CrashBest/results/eval_plots_cv/fold_03/global_model_fold_03.parquet"
+MODEL_PATH="/datasets/nmaja/CrashBest/results/eval_plots_cv/fold_04/global_model_fold_04.parquet"
 CLASSIFIER="global_model_fold_05.classifier.pkl"
 SCENE_CSV="scene_index.csv"
 
